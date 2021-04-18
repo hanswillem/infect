e7e1034f5ac67130ef14018c8697cb485f094504
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2021-04-18 15:47:56 +0200

    Update infect.py

--- infect.py
+++ infect.py
@@ -3,22 +3,23 @@
 # how to use:
 #
 # REPLACE RANDOM CHUNK OF BYTES
-# python infect.py kick.wav epilepticfit.csv -m 1 -c 45 -i 10 -f 5
+# python infect.py kick.wav epilepticfit.csv -m 1 -c 1024 -i 10 -f 5
 # -m = mode 1
-# -c = maximum lenght of the chunk of bytes (the actual chunk will be random each itteration)
+# -n = maximum lenght of the chunk of bytes (the actual chunk will be random each itteration)
 # -i = amount of itterations
 # -f = amount of files that will be exported
 # 
 # REPLACE RANDOM BYTES
 # python infect.py kick.wav epilepticfit.csv -m 2 -r 1000 -f 5
 # -m = mode 2
-# -r = amount of random bytes to replace
+# -n = amount of random bytes to replace
 # -f = amount of files that will be exported
 #
 # REPLACE EVERY N BYTES (works good for PNG's and MP4)
 # python infect.py kick.wav epilepticfit.csv -m 3 -n 512
 # -m = mode 3
 # -n = replace every n bytes
+# -f = amount of files that will be exported
 
 import argparse
 import random
@@ -26,26 +27,27 @@ import os
 
 # ----------------------------- glitch functions -----------------------------
 
-def main():
+def main():    
+    # MDOE 1
     def replaceRandomChunksOfBytes(c, n):
-        c = min(100, c)
         for t in range(n):
-            # get bytes
-            chunk = random.randint(1, c) / 100
-            l = int(chunk * len(fileBioList))
-            pos = random.randint(0, len(fileBioList) - l)
-            bioBytes = [fileBioList[pos + i] for i in range(l)]
-            # replace bytes
+            # get chunk of bytes
+            chunk = random.randint(1, c)
+            pos = random.randint(0, len(fileBioList) - chunk)
+            bioBytes = [fileBioList[pos + i] for i in range(chunk)]
+            # replace 
             pos = random.randint(header_length, len(fileBaseList))
             maxLength = len(fileBaseList) - pos
-            l = min(l, maxLength)
+            l = min(chunk, maxLength)
             fileResultList[pos:pos + l] = bioBytes
 
+    # MODE 2
     def replaceRandomBytes(n):
         for i in range(n):
             pos = random.randint(header_length, len(fileBaseList) - header_length)
             fileResultList[pos] = random.choice(fileBioList)
 
+    # MODE 3
     def replaceEveryNBytes(n):
         for i in range(len(fileBaseList)):
             if i > header_length and i % n == 0:
@@ -64,9 +66,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("fileBase", help = "The file you want to infect.", type = str)
     parser.add_argument("fileBio", help = "The file with biodata.", type = str)
-    parser.add_argument("-r", "--randomBytes", default = 1, help = "Number of bytes to replace.", type = int)
-    parser.add_argument("-n", "--everyNBytes", default = 512, help = "Modulo number of bytes to replace.", type = int)
-    parser.add_argument("-c", "--chunk", default = 25, help = "An int between 0 and 100 that describes the size of the chunk of bytes in percentage", type = int)
+    parser.add_argument("-n", "--num", default = 512, help = "number.", type = int)
     parser.add_argument("-f", "--numFiles", default = 1, help = "The amount of files that will be created.", type = int)
     parser.add_argument("-i", "--itteration", default = 1, help = "The amount of times the function will be repeated.", type = int)
     parser.add_argument("-m", "--mode", default = 1, help = "1 = Replace chunk of bytes. | 2 = Replace random bytes. | 3 = Replace every n byte.", type = int)
@@ -88,19 +88,20 @@ def main():
     if args.mode == 1:
         print("Mode 1: replace chunks of bytes")
         for i in range(args.numFiles):
-            replaceRandomChunksOfBytes(args.chunk, args.itteration)
+            replaceRandomChunksOfBytes(args.num, args.itteration)
             saveResult(i)
+            fileResultList = fileBaseList.copy()
         print("finshed!")
     elif args.mode == 2:
         print("Mode 2: replace random bytes")
         for i in range(args.numFiles):
-            replaceRandomBytes(args.randomBytes)
+            replaceRandomBytes(args.num)
             saveResult(i)
         print("finshed!")
     elif args.mode == 3:
         print("Mode 3: replace every n byte")
         for i in range(args.numFiles):
-            replaceEveryNBytes(args.everyNBytes)
+            replaceEveryNBytes(args.num)
             saveResult(i)
         print("finshed!")
     else:
